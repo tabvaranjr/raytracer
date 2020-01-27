d5270e5be3f5ac246370e630193f1758687df317
Patrick Clément-Bonhomme <patrick.cb@gmail.com>
2020-01-27 16:04:01 -0500

    Added placeholders for tests in Chapter 1.

--- tests/tests-tuple.cpp
+++ tests/tests-tuple.cpp
@@ -1,5 +1,5 @@
 #include <ostream>
-#include "catch.hpp"
+#include "catch2/catch.hpp"
 
 const double EPSILON = 1e-6;
 bool isEqual(double a, double b)
@@ -189,3 +189,226 @@ SCENARIO("vector() create tuples with w=0")
         }
     }
 }
+
+SCENARIO("Adding two tuples")
+{
+    GIVEN("a1 <- tuple(3, -2, 5, 1)")
+    AND_GIVEN("a2 <- tuple(-2, 3, 1, 0)")
+    {
+        THEN("a1 + a2 = tuple(1, 1, 6, 1)")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Subtracting two points")
+{
+    GIVEN("p1 <- point(3, 2, 1)")
+    AND_GIVEN("p2 <- point(5, 6, 7)")
+    {
+        THEN("p1 - p2 = vector(-2, -4, -6)")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Substracting a vector from a point")
+{
+    GIVEN("p <- point(3, 2, 1)")
+    AND_GIVEN("v <- vector(5, 6, 7)")
+    {
+        THEN("p - v = point(-2, -4, -6)")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Substracting two vectors")
+{
+    GIVEN("v1 <- vector(3, 2, 1)")
+    AND_GIVEN("v2 <- vector(5, 6, 7)")
+    {
+        THEN("v1 - v2 = vector(-2, -4, -6)")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Substracting a vector from the zero vector")
+{
+    GIVEN("zero <- vector(0, 0, 0)")
+    AND_GIVEN("v <- vector(1, -2, 3)")
+    {
+        THEN("zero - v <- vector(-1, 2, -3)")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Negating a tuple")
+{
+    GIVEN("a <- tuple(1, -2, 3, -4)")
+    {
+        THEN("-a <- tuple(-1, 2, -3, 4)")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Multiplying a tuple by a scalar")
+{
+    GIVEN("a <- tuple(1, -2, 3, -4)")
+    {
+        THEN("a * 3.5 = tuple(3.5, -7, 10.5, -14)")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Multiplying a tuple by a fraction")
+{
+    GIVEN("a <- tuple(1, -2, 3, -4)")
+    {
+        THEN("a * 0.5 = tuple(0.5, -1, 1.5, -2)")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Dividing a tuple by a scalar")
+{
+    GIVEN("a <- tuple(1, -2, 3, -4)")
+    {
+        THEN("a / 2 = tuple(0.5, -1, 1.5, -2)")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Computing the magnitude of vector(1, 0, 0)")
+{
+    GIVEN("v <- vector(1, 0, 0)")
+    {
+        THEN("magnitude(v) = 1")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Computing the magnitude of vector(0, 1, 0)")
+{
+    GIVEN("v <- vector(0, 1, 0)")
+    {
+        THEN("magnitude(v) = 1")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Computing the magnitude of vector(0, 0, 1)")
+{
+    GIVEN("v <- vector(0, 0, 1)")
+    {
+        THEN("magnitude(v) = 1")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Computing the magnitude of vector(1, 2, 3)")
+{
+    GIVEN("v <- vector(1, 2, 3)")
+    {
+        THEN("magnitude(v) = √14")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Computing the magnitude of vector(-1, -2, -3)")
+{
+    GIVEN("v <- vector(-1, -2, -3)")
+    {
+        THEN("magnitude(v) = √14")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Normalizing vector(4, 0, 0) gives (1, 0, 0)")
+{
+    GIVEN("v <- vector(4, 0, 0)")
+    {
+        THEN("normalize(v) = vector(1, 0, 0)")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("Normalizing vector(1, 2, 3)")
+{
+    GIVEN("v <- vector(1, 2, 3)")
+    {
+        THEN("normalize(v) = approximately vector(0.26726, 0.53452, 0.80178)")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("The magnitude of a normalized vector")
+{
+    GIVEN("v <- vector(1, 2, 3)")
+    {
+        WHEN("norm <- normalize(v)")
+        THEN("magnitude(norm) = 1")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("The dot product of two tuples")
+{
+    GIVEN("a <- vector(1, 2, 3)")
+    AND_GIVEN("b <- vector(2, 3, 4)")
+    {
+        THEN("dot(a, b) = 20")
+        {
+            FAIL();
+        }
+    }
+}
+
+SCENARIO("The cross product of two vectors")
+{
+    GIVEN("a <- vector(1, 2, 3)")
+    AND_GIVEN("b <- vector(2, 3, 4)")
+    {
+        THEN("cross(a, b) = vector(-1, 2, -1)")
+        {
+            FAIL();
+        }
+        
+        AND_THEN("cross(b, a) = vector(1, -2, 1)")
+        {
+            FAIL();
+        }
+    }
+}
+
